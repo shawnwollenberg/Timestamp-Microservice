1c1e943661abece18b7f6c3d5dba1a4ce3f8399b
shawnwollenberg <shawn@wallyweb.com>
2016-10-10 21:11:40 +0000

    RD1

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+var moment = require('moment');
+app.get('/:timeparam', function(req, res){
+  var xDate=req.params.timeparam;
+  var xTestDate = moment(xDate, 'MMMM DD, YYYY', true);
+  //if(xDate instanceof Date){
+    if (!xTestDate.isValid()){
+      xTestDate = moment.unix(xDate);
+    }
+  //    var xUnix = Date.parse(xDate);
+  //  var xOutput='{ "unix":' + xUnix + ', "natural":"' + xStrDate + '" }';
+  //  var xOutput='{ "unix":null, "natural":null }';  
+  if (!xTestDate.isValid()) {
+    res.json({
+      'humanReadable': null,
+      'unix': null
+    });
+  }else{
+    res.json({
+      'humanReadable': xTestDate.format('MMMM DD, YYYY'),
+      'unix': xTestDate.format('X')
+  });
+ }
+})
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
